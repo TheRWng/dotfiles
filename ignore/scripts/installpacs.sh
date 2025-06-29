5ed0a4050012e92afa37be2bc743e6dfbfc3a216
TheRWng <rwmain07@gmail.com>
2025-06-30 04:26:47 +0800

    nvim

--- ignore/scripts/installpacs.sh
+++ ignore/scripts/installpacs.sh
@@ -2,70 +2,79 @@
 set -e
 
 packages=(
-	atuin
-	bat
-	blueberry
-	btop
-	cliphist
-	cpupower
-	dunst
-	egl-wayland
-	eza
-	fastfetch
-	fd
-	fzf
-	ghostty
-	gitleaks
-	hypridle
-	hyprland
-	hyprlock
-	hyprpaper
-	hyprpolkitagent
-	hyprshot
-	hyprsunset
-	imv
-	jq
-	less
-	libnewt
-	libnotify
-	mpv
-	ncdu
-	neovim
-	nvidia-dkms
-	nvidia-utils
-	openssh
-	pavucontrol
-	pipewire-pulse
-	polkit
-	qt5-wayland
-	qt6-wayland
-	ripgrep
-	rofi-wayland
-	stow
-	timeshift
-	tldr
-	tmux
-	ttf-jetbrains-mono-nerd
-	udiskie
-	udisks2
-	waybar
-	wireplumber
-	wl-clipboard
-	wl-clip-persist
-	xdg-desktop-portal-hyprland
-	yazi
-	zoxide
-	zsh
-	# gamescope       # For problems with steam/proton games
-	# lib32-nvidia-utils
-	# steam
-	# ttf-font-awesome
-	# hyprpm plugin if using hyprland plugins
+  atuin
+  bat
+  blueberry
+  btop
+  cliphist
+  cpupower
+  dunst
+  egl-wayland
+  eza
+  fastfetch
+  fd
+  fzf
+  ghostty
+  ghostscript
+  gitleaks
+  hypridle
+  hyprland
+  hyprlock
+  hyprpaper
+  hyprpolkitagent
+  hyprshot
+  hyprsunset
+  imagemagick
+  imv
+  jq
+  less
+  libnewt
+  libnotify
+  lua51 # For lazyvim
+  luarocks
+  mermaid-cli
+  mpv
+  ncdu
+  neovim
+  nodejs
+  nvidia-dkms
+  nvidia-utils
+  openssh
+  pavucontrol
+  pipewire-pulse
+  polkit
+  qt5-wayland
+  qt6-wayland
+  ripgrep
+  rofi-wayland
+  stow
+  tectonic
+  timeshift
+  tldr
+  tmux
+  tree-sitter
+  tree-sitter-cli
+  ttf-jetbrains-mono-nerd
+  udiskie
+  udisks2
+  waybar
+  wireplumber
+  wl-clipboard
+  wl-clip-persist
+  xdg-desktop-portal-hyprland
+  yazi
+  zoxide
+  zsh
+  # gamescope       # For problems with steam/proton games
+  # lib32-nvidia-utils
+  # steam
+  # ttf-font-awesome
+  # hyprpm plugin if using hyprland plugins
 )
 
 # Check if package is installed
 is_installed() {
-    pacman -Qi "$1" &>/dev/null
+  pacman -Qi "$1" &>/dev/null
 }
 
 # Update package database
@@ -73,16 +82,12 @@ echo "Updating package database..."
 sudo pacman -Sy
 
 for pkg in "${packages[@]}"; do
-    if pacman -Qi "$pkg" &>/dev/null; then
-        echo "[SKIPPED] $pkg is already installed"
-    else
-        echo "[INSTALLING] $pkg..."
-        sudo pacman -S --needed --noconfirm "$pkg" || echo "[WARNING] Failed to install $pkg"
-    fi
+  if pacman -Qi "$pkg" &>/dev/null; then
+    echo "[SKIPPED] $pkg is already installed"
+  else
+    echo "[INSTALLING] $pkg..."
+    sudo pacman -S --needed --noconfirm "$pkg" || echo "[WARNING] Failed to install $pkg"
+  fi
 done
 
 echo "Installation complete. Reboot recommended for Hyprland."
-
-# bind = SUPER, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy
-#
-# then sudo mkinitcpio -P and reboot
--- ignore/scripts/installpars.sh
+++ ignore/scripts/installpars.sh
@@ -2,14 +2,14 @@
 set -e
 
 packages=(
-    asdf-vm
-    wlogout
-    zen-browser-bin
+  asdf-vm
+  wlogout
+  zen-browser-bin
 )
 
 # Check if package is installed
 is_installed() {
-    paru -Qi "$1" &>/dev/null
+  paru -Qi "$1" &>/dev/null
 }
 
 # Update package databases
@@ -17,12 +17,12 @@ echo "Updating package databases..."
 paru -Sy --noconfirm
 
 for pkg in "${packages[@]}"; do
-    if is_installed "$pkg"; then
-        echo "[SKIPPED] $pkg is already installed"
-    else
-        echo "[INSTALLING] $pkg..."
-        paru -S --needed --noconfirm "$pkg" || echo "[WARNING] Failed to install $pkg"
-    fi
+  if is_installed "$pkg"; then
+    echo "[SKIPPED] $pkg is already installed"
+  else
+    echo "[INSTALLING] $pkg..."
+    paru -S --needed --noconfirm "$pkg" || echo "[WARNING] Failed to install $pkg"
+  fi
 done
 
 echo "Installation complete. Reboot recommended for Hyprland."
